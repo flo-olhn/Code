9e21d4d78d95d5a478c3ccba8393762c16f61978
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-09-26 15:47:44 +0200

    Start of Cookcademy project

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Florian Ouilhon on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipesListView()
+        }
+    }
+}
