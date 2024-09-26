aa9474be7caedede90cb07e18eb1743b9b4ac0f5
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-09-26 21:46:03 +0200

    Add feature to let user add new recipes + handling empty recipes

--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -11,6 +11,9 @@ struct RecipesListView: View {
     @EnvironmentObject private var recipeData: RecipeData
     let category: MainInformation.Category
     
+    @State private var isPresenting = false
+    @State private var newRecipe = Recipe()
+    
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
     
@@ -23,6 +26,36 @@ struct RecipesListView: View {
             .foregroundStyle(listTextColor)
         }
         .navigationTitle(navigationTitle)
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    isPresenting = true
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+        })
+        .sheet(isPresented: $isPresenting, content: {
+            NavigationView {
+                ModifyRecipeView(recipe: $newRecipe)
+                    .toolbar(content: {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Dismiss") {
+                                isPresenting = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            if newRecipe.isValid {
+                                Button("Add") {
+                                    recipeData.add(recipe: newRecipe)
+                                    isPresenting = false
+                                }
+                            }
+                        }
+                    })
+                    .navigationTitle("Add a New Recipe")
+            }
+        })
     }
 }
 
